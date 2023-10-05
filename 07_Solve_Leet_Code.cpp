8d33cecd5a3ade86b50bec8b6ccd77239e7107ac
Fida <722003hussainfida@gmail.com>
2023-10-05 17:21:45 +0530

    lecture_07

---
+++ 07_Solve_Leet_Code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //***************************************************************************************************************************
+    // Q1. Reverse Integer ?
+
+https: // leetcode.com/problems/reverse-integer/
+
+    //***************************************************************************************************************************
+    // Q2. Complement of base 10 Integer ?
+
+https: // leetcode.com/problems/complement-of-base-10-integer/
+
+    //***************************************************************************************************************************
+    // Q3. Complement of base 10 Integer ?
+
+https: // leetcode.com/problems/power-of-two/
+
+    return 0;
+}
